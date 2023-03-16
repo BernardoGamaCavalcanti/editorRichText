d8600b81cc39996db1256acbddeda0f112eaa1c1
Bernardo Cavalcanti <bernardogamacavalcanti@gmail.com>
2023-03-16 19:28:24 -0300

    adição do pincel preto

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 //Cria um array com todos elementos que possuem a classe btn (todos os nossos botões tem essa classe)
 const elements = document.querySelectorAll('.btn')
+console.log(elements)
 
 //Depois de criado o array é hora de percorrê-lo usando forEach ("para cada, faça...") e aqui damos o nome de element para o parametro que vamos receber
 elements.forEach(element => {
@@ -10,6 +11,8 @@ elements.forEach(element => {
         //criamos command para guardar o valor que está dentro de dataset e de lá pegamos o valor do atributo personalizado data-element: bold, italic, underline...
         let command = element.dataset['element']
 
+        //Aqui criamos condicionais que vão comparar o valor de command (que recebeu o que estava dentro de data-element) do botão pressionado e cada um faz algo diferente
+        //sobre document.execCommand: https://developer.mozilla.org/pt-BR/docs/Web/API/Document/execCommand
         if(command == 'createLink') {
             let url = prompt('Insira o link', 'http://')
             document.execCommand(command, false, url)
@@ -37,6 +40,13 @@ elements.forEach(element => {
         else if (command == 'colorGreen') {
             // Colorize text
            document.execCommand("ForeColor", false, "green");
+           // Set design mode to off
+           document.designMode = "off";
+        }
+
+        else if (command == 'colorBlack') {
+            // Colorize text
+           document.execCommand("ForeColor", false, "black");
            // Set design mode to off
            document.designMode = "off";
         }
